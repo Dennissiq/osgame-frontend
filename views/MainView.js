0f8216341de21ed89acd9b7acda91eebdfd1cd8e
Dennis Siqueira <ti.dennissiqueira@gmail.com>
2019-03-30 14:05:34 -0300

    Add preemptive RR Scheduler

--- views/MainView.js
+++ views/MainView.js
@@ -26,6 +26,8 @@ function loadAPI() {
 
         // Set scheduler name
         $(document).ready(function () {
+
+            console.log(response)
             if (response === 'Round Robin 1 Quantum NP'){
                 $('#textH1').text('Escalonador Round Robin 1 Quantum')
                 $('#textH3').text("Non preemptive")
@@ -61,13 +63,15 @@ function loadAPI() {
                         $box2 = $('#box2'),
                         $box3 = $('#box3'),
                         $box4 = $('#box4'),
+                        $halfBox = $('#halfBox')
                         $title = $('#textH1'),
                         $subtitle = $('#textH3'),
                         $repeatAnimation = $('#repeatAnimation'),
                         tl = new TimelineLite();
 
                     // animation timeline
-                    tl.from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })
+                    tl.to($halfBox, 0, { x: '150', autoAlpha: 0 })
+                        .from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })   
                         .from($box, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box2, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box3, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
@@ -89,13 +93,15 @@ function loadAPI() {
                         $box3 = $('#box3'),
                         $box4 = $('#box4'),
                         $title = $('#textH1'),
-                        $subtitle = $('#textH3')
+                        $subtitle = $('#textH3'),
+                        $halfBox = $('#halfBox'),
                         $repeatAnimation = $('#repeatAnimation')
                         $boxes = $('.box'),
                         tl = new TimelineLite();
 
                     // animation timeline
-                    tl.from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })
+                    tl.to($halfBox, 0, { x: '150', autoAlpha: 0 })
+                        .from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box2, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box3, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
@@ -123,6 +129,7 @@ function loadAPI() {
                         $box2 = $('#box2'),
                         $box3 = $('#box3'),
                         $box4 = $('#box4'),
+                        $halfBox = $('#halfBox'),
                         $title = $('#textH1'),
                         $subtitle = $('#textH3')
                         $repeatAnimation = $('#repeatAnimation')
@@ -130,7 +137,8 @@ function loadAPI() {
                         tl = new TimelineLite();
 
                     // animation timeline
-                    tl.from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })
+                    tl.to($halfBox, 0, { x: '150', autoAlpha: 0 })
+                        .from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })  
                         .from($box, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box2, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box3, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
@@ -157,6 +165,7 @@ function loadAPI() {
                         $box2 = $('#box2'),
                         $box3 = $('#box3'),
                         $box4 = $('#box4'),
+                        $halfBox = $('#halfBox')
                         $title = $('#textH1'),
                         $subtitle = $('#textH3')
                         $repeatAnimation = $('#repeatAnimation')
@@ -164,7 +173,8 @@ function loadAPI() {
                         tl = new TimelineLite();
 
                     // animation timeline
-                    tl.from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })
+                    tl.to($halfBox, 0, { x: '150', autoAlpha: 0 })
+                    .from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })  
                         .from($box, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box2, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
                         .from($box3, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
@@ -207,7 +217,7 @@ function loadAPI() {
                         .to($box2, 0, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
                         .to($box, 0, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
                         .to($box, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 1 })
-                        .to($box, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+                        .to($box, 0.5, { x: '-450', ease: Power4.easeInOut, autoAlpha: 0 })
                         // .to($box, 0.5, { y: '-100', ease: Power4.easeInOut })
                         // .to($box, 0.5, { x: '0', ease: Power4.easeInOut })
                         // .to($box, 0.5, { y: '-30', ease: Power4.easeInOut })
@@ -217,6 +227,97 @@ function loadAPI() {
                         .from($repeatAnimation, 0.5, { autoAlpha: 0, ease: Power4.easeInOut });
                         
                 } animationRrOneNp()
+                break;
+            case 'Round Robin 1 Quantum P':
+                function animationRrOneP(){
+
+                    if (response === 'Round Robin 1 Quantums') {
+                        $('#priorityBox1').text("1")
+                        $('#priorityBox2').text("2")
+                        $('#priorityBox3').text("1")
+                        $('#priorityBox4').text("3")
+                    }
+
+                    let $box = $('#box'),
+                        $box2 = $('#box2'),
+                        $box3 = $('#box3'),
+                        $box4 = $('#box4'),
+                        $title = $('#textH1'),
+                        $subtitle = $('#textH3'),
+                        $halfBox = $('#halfBox')
+                        $repeatAnimation = $('#repeatAnimation')
+                        $boxes = $('.box'),
+                        tl = new TimelineLite();
+
+                    // animation timeline
+                    tl.to($halfBox, 0, { x: '-350', autoAlpha: 0 })
+                        .from([$title, $subtitle], 0.7, { x: '150', y: '-40', autoAlpha: 0, ease: Power4.easeInOut })
+                        .from($box, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
+                        .from($box2, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
+                        .from($box3, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
+                        .from($box4, 0.5, { x: '150', autoAlpha: 0, ease: Power4.easeInOut })
+                        .to($box, 0.5, {x: '-365', ease: Power4.easeInOut})
+                        .to($box, 0.5, {x: '-365', ease: Power4.easeInOut, autoAlpha: 0})
+                        // halfbox
+                        .to($halfBox, 0.5, {x: '-350', ease: Power4.easeInOut, autoAlpha:1})
+                        .to($halfBox, 0.5, {y: '-100', ease: Power4.easeInOut, autoAlpha:1})
+                        .to($box, 0, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+                        // box2
+                        .to($box2, 1, { x: '-332', ease: Bounce.easeOut })
+                        .to($box2, 1, { x: '-388', ease: Power4.easeOut })
+                        .to($box2, 0.5, { x: '-388', ease: Power4.easeInOut, autoAlpha: 0 })
+                        // back to cpu position
+                        .to($box2, 0, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+                        .to($box, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 1 })
+
+                        .to($halfBox, 0.5, {x: '310', ease: Power4.easeInOut, autoAlpha:1})
+                        .to($halfBox, 0.5, {y: '-30', ease: Power4.easeInOut, autoAlpha:1})
+
+
+                        .to($box, 1, {y: '-100', ease: Power4.easeInOut})
+                        .to($box, 1, {x: '365', ease: Power4.easeInOut})
+
+                        .to($box, 0.5, {y: '-30', ease: Power4.easeInOut})
+                        .to($box3, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+                        
+                        .to($box4, 0.5, {x:'-200', ease: Power4.easeInOut})
+                        .to($halfBox, 0.5, { x: '0', ease: Power4.easeInOut, autoAlpha: 1 })
+                        .to($box, 0.5, {x:'65', ease: Power4.easeInOut})
+                        // box4
+                        .to($box4, 1, { x: '-332', ease: Bounce.easeOut })
+                        .to($box4, 1, { x: '-388', ease: Power4.easeOut })
+                        .to($box4, 0.5, { x: '-388', ease: Power4.easeOut, autoAlpha: 0 })
+                        .to($box2, 0.5, { x: '-332', ease: Power4.easeOut, autoAlpha: 1 })
+                        .to($box2, 1, { y: '-100', ease: Power4.easeInOut })
+                        .to($box2, 1, { x: '130', ease: Power4.easeInOut })
+                        .to($box2, 1, { y: '-30', ease: Power4.easeInOut })
+                        // last box 2 (splited - box 1)
+                        .to($halfBox, 0.5, { x: '-350', ease: Power4.easeInOut, autoAlpha: 0 })
+                        .to($box, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+
+                        .to($box2, 0.5, { x: '-200', ease: Power4.easeInOut })
+                        .to($box, 0.5, { x: '0', ease: Power4.easeInOut })
+
+                        .to($box2, 1, { x: '-332', ease: Bounce.easeOut })
+                        .to($box2, 1, { x: '-388', ease: Power4.easeOut })
+                        .to($box2, 0.5, { x: '-388', ease: Power4.easeInOut, autoAlpha: 0 })
+                        // back to cpu position
+                        .to($box2, 0, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+
+                        .to($box, 0, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })
+                        .to($box, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 1 })
+                        // .to($box, 0.5, { y: '-100', ease: Power4.easeInOut, autoAlpha: 1 })
+
+                        .to($box, 0.5, { x: '-450', ease: Power4.easeInOut, autoAlpha: 0 })
+                        // .to($box, 0.5, { y: '-100', ease: Power4.easeInOut })
+                        // .to($box, 0.5, { x: '0', ease: Power4.easeInOut })
+                        // .to($box, 0.5, { y: '-30', ease: Power4.easeInOut })
+                        // .to($box, 0.5, { x: '-332', ease: Power4.easeInOut, autoAlpha: 0 })                                     
+                        
+                        // .to($box4, 2.2, {x: '-332', ease: Power4.easeInOut})
+                        .from($repeatAnimation, 0.5, { autoAlpha: 0, ease: Power4.easeInOut });
+                        
+                } animationRrOneP()
                 break;
             // case 'Round Robin 2 Quantums NP':
             //     function animationRrTwo(){
